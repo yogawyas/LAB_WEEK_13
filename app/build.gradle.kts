4b83e9156f2df317945f4bf411ad9fe5a93b950e
Yoga Wyas <49479642+yogawyas@users.noreply.github.com>
2025-11-24 16:02:35 +0700

    Commit No. 1 – Init template

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.squareup.moshi:moshi:1.15.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
     implementation(libs.androidx.recyclerview)
     implementation(libs.glide)
     implementation(libs.retrofit)
